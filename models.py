7d318034f482430c0137fbb8f57d8dfaca967bcd
Tom Boone <boone@wrlc.org>
2023-09-26 13:07:34 -0400

    add user-day combo constraint

--- models.py
+++ models.py
@@ -242,6 +242,9 @@ class UserDay(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.ForeignKey(User.id))
     day = db.Column(db.Integer, nullable=False)
+    __table_args__ = (
+        db.UniqueConstraint('user', 'day', name='_user_day_uc'),
+    )
 
     # Constructor
     def __repr__(self):
